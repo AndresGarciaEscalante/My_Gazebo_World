d868a4703c911c2fa8247eb294a8c4c2d436f8a1
AndresGarciaEscalante <andres_ge@hotmail.es>
2020-01-08 14:56:59 -0600

    Project completed succesfully

---
+++ script/welcome.cpp
@@ -0,0 +1,17 @@
+#include <gazebo/gazebo.hh>
+
+namespace gazebo
+{
+  class WorldPluginMyHome : public WorldPlugin
+  {
+    public: WorldPluginMyHome() : WorldPlugin()
+            {
+              printf("Welcome to my new home!\n");
+            }
+
+    public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
+            {
+            }
+  };
+  GZ_REGISTER_WORLD_PLUGIN(WorldPluginMyHome)
+}
